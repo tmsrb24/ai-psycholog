9a945e2b9ea16e674a0f58fda3b246a5a848b6ab
tmsrb24 <tomas.srb@luxonis.com>
2025-06-01 14:07:33 +0200

    Explicitly set localePath in next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -7,7 +7,7 @@ module.exports = {
   // Optional: react-i18next options
   // react: { useSuspense: false }
   // Optional: reload on change in dev mode
-  // reloadOnPrerender: process.env.NODE_ENV === 'development',
+  reloadOnPrerender: process.env.NODE_ENV === 'development', // Odkomentováno pro jistotu
   // Optional: locale path
-  // localePath: typeof window === 'undefined' ? require('path').resolve('./public/locales') : '/locales',
+  localePath: require('path').resolve('./public/locales'), // Odkomentováno a upraveno
 };
