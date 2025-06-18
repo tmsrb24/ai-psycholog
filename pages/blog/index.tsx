f0c3e35f222fc2c9aba8dc5605b906164cb317bb
tmsrb24 <tomas.srb@luxonis.com>
2025-06-18 11:41:51 +0200

    fix: Correct import path in blog/index

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import Layout from '../../components/Layout';
+import Layout from '../../components/layouts/Layout';
 import { GetStaticProps, InferGetStaticPropsType } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { supabase } from '../../lib/supabaseClient';
