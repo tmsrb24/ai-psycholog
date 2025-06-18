19afbc5cff2ecb0305e265fd4281aae1c0bb6ed3
tmsrb24 <tomas.srb@luxonis.com>
2025-06-18 11:47:57 +0200

    fix: Correct import path in gdpr

--- pages/gdpr.tsx
+++ pages/gdpr.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Layout from '../components/Layout';
+import Layout from '../components/layouts/Layout';
 import { 
   FaLock, FaUserCheck, FaDatabase, FaEnvelopeOpenText, FaUserLock, 
   FaUserSlash, FaClipboardCheck, FaBalanceScale, FaShieldAlt,
