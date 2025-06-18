7d247f49911ed19a4a67ab54892204df8bfaf5eb
tmsrb24 <tomas.srb@luxonis.com>
2025-06-18 11:29:29 +0200

    fix: Correct import path in admin/users

--- pages/admin/users.tsx
+++ pages/admin/users.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Layout from '../../components/Layout';
+import Layout from '../../components/layouts/Layout';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 // UserProfileData zde není přímo potřeba, definujeme AdminUser explicitně
