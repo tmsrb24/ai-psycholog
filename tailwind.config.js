1c8af87ac9116e9ef9151ef1fec5dbe6d9b07503
tmsrb24 <tomas.srb@luxonis.com>
2025-06-26 22:14:26 +0200

    fix: Oprava buildu po aktualizaci flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './node_modules/flowbite-react/**/*.js',
   ],
   darkMode: 'class',
   theme: {
@@ -42,10 +43,5 @@ module.exports = {
     require('@tailwindcss/typography'),
     require('flowbite/plugin'),
     require('daisyui'),
-  ],
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './node_modules/flowbite-react/**/*.js',
   ],
 };
