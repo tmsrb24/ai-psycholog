eb6f57711af4ce7b68b7e2ae58b2aa9902cb49d9
tmsrb24 <tomas.srb@luxonis.com>
2025-06-26 22:31:04 +0200

    fix: Update Stripe API version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -2,7 +2,7 @@ import { Stripe } from 'stripe';
 
 // Initialize Stripe with the secret key from environment variables
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-  apiVersion: '2025-04-30.basil', // Use the latest API version
+  apiVersion: '2025-05-28.basil', // Use the latest API version
 });
 
 export default stripe;
